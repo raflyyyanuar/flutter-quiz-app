3b2eed4f9e2da3ba240a2da90b8d765b607b4d0b
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-17 10:18:21 +0700

    separated start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: mainScreen(),
+        body: StartScreen(),
         backgroundColor: const Color.fromARGB(255, 64, 0, 153),
       ),
-    ),
-  );
-}
-
-Widget mainScreen() {
-  return Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          height: 250,
-        ),
-        
-        const SizedBox(
-          height: 45,
-        ),
-        
-        const Text(
-          "Learn Flutter the fun way!",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-          ),
-        ),
-        
-        const SizedBox(
-          height: 15,
-        ),
-        
-        OutlinedButton(
-          onPressed: () {},
-          child: const Text(
-            "Start Quiz",
-            style: TextStyle(
-              fontSize: 12,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ],
     ),
   );
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            height: 250,
+          ),
+          const SizedBox(
+            height: 45,
+          ),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text(
+              "Start Quiz",
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
